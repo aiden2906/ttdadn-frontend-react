d221cb14bf4004b077c9fa58d4f5341ffbff3290
Tran Thanh Quang <quang.tran290699@hcmut.edu.vn>
2020-06-08 14:23:58 +0700

    update-frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
+import Chart from './components/chart';
 const ENDPOINT = 'http://127.0.0.1:4000';
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       console.log(data);
     });
   }, []);
-  return <p></p>;
+  return <Chart></Chart>;
 };
 
 export default App;
---
+++ src/components/chart/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Chart = () => {
+  return <p>tranthanhquang</p>;
+};
+
+
+export default Chart;
