e49e4ec1ae0c51bb6b17bf121703e58903522ed6
tran thanh quang <quang.tran290699@gmail.com>
2020-07-29 09:33:46 +0700

    change url swagger api

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <a href="#" onClick={()=> window.open("https://gitlab.com/quangtranthanh", "_blank")}>Gitlab Project</a>
           </div>
           <div>
-          <a href="#" onClick={()=> window.open("http://localhost:4000/docs.api", "_blank")}>Bộ API</a>
+          <a href="#" onClick={()=> window.open("http://40.87.101.198:4000/docs.api/", "_blank")}>Bộ API</a>
           </div>
         </div>
       </div>
