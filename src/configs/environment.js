7c306788452ffe55d29a5fb9d578184ce526eae2
tran thanh quang <quang.tran290699@gmail.com>
2020-07-29 09:33:09 +0700

    change endpoint

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
-
+const path = require('path')
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
-const ENDPOINT = process.env.ENDPOINT || 'http://localhost:4000';
+const ENDPOINT = process.env.ENDPOINT || 'http://40.87.101.198:4000';
 const SOCKET_NOTIFICATION = process.env.SOCKET_NOTI || 'notification';
 const SOCKET_COUNT_NOTIFICATION = process.env.SOCKET_COUNT_NOTI || 'count-notification';
 const SOCKET_HUMI = process.env.SOCKET_HUMI || 'sensorChange';
